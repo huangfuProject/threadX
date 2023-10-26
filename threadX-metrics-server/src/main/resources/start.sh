bd87c27ffb768ff32ef47121a1b6f842c9ad9c9d
huangfuProject <huangfu_project@163.com>
2023-10-26 16:56:19 +0800

    部署脚本编写

--- threadX-metrics-server/src/main/resources/start.sh
+++ threadX-metrics-server/src/main/resources/start.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
+# 获取脚本所在目录的绝对路径
+SCRIPT_DIR="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")" && pwd)"
 
 # 定义应用程序的相关信息
 APP_NAME="threadX-metrics-server"
-VERSION="1.0"  # 请替换为你的应用程序版本号
+VERSION="1.0-SNAPSHOT"  # 请替换为你的应用程序版本号
 
 # 定义应用程序的路径
-APP_DIR="../lib"
-CONFIG_DIR="../conf"
-LOG_DIR="./logs"
+APP_DIR="$SCRIPT_DIR/lib"
+LOG_DIR="$SCRIPT_DIR/logs"
+CONFIG_DIR="$SCRIPT_DIR/conf"
 JAR_FILE="$APP_DIR/$APP_NAME-${VERSION}.jar"
 CONFIG_FILE="$CONFIG_DIR/application.properties"
 LOG_CONFIG_FILE="$CONFIG_DIR/logback-spring.xml"
+JVM_XMX="2g"
 
 # 创建日志目录（如果不存在）
 mkdir -p $LOG_DIR
 
-# 定义JVM参数
-# 请根据你的需求和硬件配置修改这些参数
-JVM_XMS="2g"
-JVM_XMX="2g"
-
-# 启动应用程序并配置日志
-java -Xms$JVM_XMS -Xmx$JVM_XMX -Dlogging.config=$LOG_CONFIG_FILE -jar $JAR_FILE --spring.config.location=file:$CONFIG_FILE >> $LOG_DIR/out.log 2>&1
+# 启动应用程序
+nohub java -Xms$JVM_XMX -Xmx$JVM_XMX -DlogPath=$LOG_DIR/app.log -Dlogging.config=$LOG_CONFIG_FILE -jar $JAR_FILE --spring.config.location=file:$CONFIG_FILE >> $LOG_DIR/out.log 2>&1
